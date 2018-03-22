d1bdb6beb94bf572bb60971de3efb175caba87e1
PiumiHR <rathnayakepiumi19@gmail.com>
2018-03-23 01:44:20 +0530

    commit 3

--- app/src/main/java/com/example/asankad7/eeg/MainActivity.java
+++ app/src/main/java/com/example/asankad7/eeg/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.asankad7.eeg;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import at.markushi.ui.CircleButton;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity {
-
-CircleButton bStartone;
+    private Toolbar toolbar;
+    CircleButton bStartone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar)findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+
         bStartone = (CircleButton) findViewById(R.id.bStartone);
-//btnSubmit = (SubmitButton) findViewById(R.id.bStartone);
         bStartone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +34,31 @@ CircleButton bStartone;
 
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorite) {
+            Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
 }
